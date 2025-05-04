e28763b0aaff58b3c916de4aa2df3f92de8d083b
Gimhani Sandareka <159515131+GimhaniSandareka@users.noreply.github.com>
2025-05-04 18:25:38 +0530

    Session 4: Added Shadcn library

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path"
+// import { fileURLToPath } from "url";
+import react from "@vitejs/plugin-react-swc"
+import { defineConfig } from "vite"
 
-// https://vite.dev/config/
+// recreate __dirname in ESM
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
